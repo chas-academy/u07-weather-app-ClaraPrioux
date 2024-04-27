32b9fec6618c7578361f6143ddde71b212265496
Clara Prioux <prioux.clara@gmail.com>
2024-04-27 18:14:24 +0200

    fixes #3

--- weather-app/src/components/geolocation/GeolocationComponent.tsx
+++ weather-app/src/components/geolocation/GeolocationComponent.tsx
@@ -23,7 +23,7 @@ const GeolocationComponent = () => {
           });
         },
         () => {
-          setStatus("Unable to retrieve your location");
+          setStatus("Oops! It seems we couldn't access your location through your browser. Please make sure to enable location sharing in your browser settings or try using another browser.");
         }
       );
     }
@@ -35,6 +35,7 @@ const GeolocationComponent = () => {
 
   return (
     <>
+      {status && <p>{status}</p>}
       {status === "Loading" && (
         <img src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!bw700" alt="Loading" className="w-1/4" />
       )}
